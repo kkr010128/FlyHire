3cf2f9430e35f6aedb457bfc5c1b9e4d67d576ec
Ocean_01 <96841065+kimyoungho010311@users.noreply.github.com>
2025-07-01 15:26:54 +0900

    feat: 직원수 조건 검색 구현완료

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import time
 import math
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 load_dotenv()
@@ -24,14 +24,33 @@ headers = {"Authorization": f"Infuser {apiKey}"}
 
 app = Flask(__name__)
 
+df = pd.read_csv("/Users/kim-youngho/git/FlyHire/data/2506_to_test.csv")# 경로를 못찾아서 절대경로로 바꿈
 
-@app.route("/")
+def filter_employee_count(target):
+    if target == "50":
+        return df[df['가입자수'] <= 50]
+    elif target == "51":
+        return df[(df['가입자수'] >= 51) & (df['가입자수'] <= 300)]
+    elif target == "301":
+        return df[(df['가입자수'] >= 301) & (df['가입자수'] <= 1000)]
+    elif target == "1001":
+        return df[(df['가입자수'] >= 1001) & (df['가입자수'] <= 10000)]
+    elif target == "10001":
+        return df[df['가입자수'] >= 10001]
+    else:
+        return df
+
+
+@app.route("/", methods=["GET","POST"])
 def table():
-    df = pd.read_csv("/Users/kim-youngho/git/FlyHire/data/2506.csv")# 경로를 못찾아서 절대경로로 바꿈
-    table_html = df.to_html(classes="table table-striped", index=False)
-    return render_template("index.html", table=table_html)
-
-
+    if request.method == "POST":
+        target = request.form.get("employeeCount")
+        filtered = filter_employee_count(target)
+        table_html = filtered.to_html(classes="table table-striped", index=False)
+    else:
+        target = "50"
+        table_html = df.to_html(classes="table table-striped", index=False)
+    return render_template("index.html", table=table_html, selected=target)
 
 
 
